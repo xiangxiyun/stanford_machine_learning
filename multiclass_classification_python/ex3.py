3e17e4ad9673ada9eaf2bbe01ba5529496f43508
xiangxiyun <xiangxy3@gmail.com>
2017-06-28 13:50:08 -0400

    Implement multi-class classification

--- multiclass_classification_python/ex3.py
+++ multiclass_classification_python/ex3.py
@@ -1,22 +1,37 @@
 import scipy.io
-from logistic_regression_python.logistic_regression import LogisticRegression
+import numpy as np
+from multiclass_classification_python.multiclassClassification import MulticlassClassification
 
 
 
 if __name__ == '__main__':
 
-    myLR = LogisticRegression()
+
 
     input_filename1 = 'data/ex3data1.mat'
     training_data = scipy.io.loadmat(input_filename1)
 
-    X = training_data['X']
-    y = training_data['y']
+    X = training_data['X'] #size: 5000*400
+    y = training_data['y'] #size: 5000*1
 
-    input_filename2 = 'data/ex3weights.mat'
-    weights = scipy.io.loadmat(input_filename2)
+    myMC = MulticlassClassification()
+    _lambda = 0.1
+    num_labels = 10
+    all_theta = myMC.training(X, y, num_labels, _lambda)
 
-    Theta1 = weights['Theta1']
-    Theta2 = weights['Theta2']
+    pred = myMC.predictOneVsAll(X, all_theta)
+    print(pred)
+    print('\nTraining Set Accuracy: ' + str( np.mean(np.array(pred == y, dtype = float)) * 100))
+
+
+
+    # input_filename2 = 'data/ex3weights.mat'
+    # weights = scipy.io.loadmat(input_filename2)
+    #
+    # Theta1 = weights['Theta1'] #size: 25*401
+    # Theta2 = weights['Theta2'] #size: 10*26
+
+
+
 
     pass
--- multiclass_classification_python/multiclassClassification.py
+++ multiclass_classification_python/multiclassClassification.py
@@ -1,6 +1,31 @@
-import scipy.io
+import numpy as np
+from logistic_regression_python.logistic_regression import LogisticRegression
 
 class MulticlassClassification:
     def __init__(self):
         pass
+
+    def training(self, X, y, num_labels, _lambda):
+        m = X.shape[0]
+        n = X.shape[1]
+
+        all_theta = np.zeros((num_labels, n + 1)) #size: 10*401
+        LR = LogisticRegression()
+
+        for i in range(num_labels):
+            new_y = np.array(y == i, dtype=int)
+            theta = LR.training(X, new_y, method = 'bfgs', _lambda= _lambda, iteration=1000)
+            all_theta[i, :] = theta.T[:]
+
+        return all_theta
+
+
+    def predictOneVsAll(self, X, all_theta):
+        m = X.shape[0]
+        new_X = np.concatenate((np.ones((m, 1), dtype=float), X), axis=1) # size: 5000*401
+        res = np.dot(new_X, all_theta.T) # size: 5000*10
+        p = np.argmax(res, axis = 1).T #size: 5000*1
+        return p
+
+
 
